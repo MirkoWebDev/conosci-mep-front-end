0def1bd0f7aa47eb41a337f030b1c23881c9dad5
Mirko <masimirko101@gmail.com>
2021-02-09 11:20:09 +0100

    index page modified, added js behaviour

--- assets/main.ts
+++ assets/main.ts
@@ -1,3 +1,16 @@
-import './main.scss';
+import './app.scss';
 
-console.log('Hello World!');
+console.log('I\'ve never used Typescript, but I\'ll try!');
+
+const btn = document.getElementById("magicButton");
+if(btn){
+    btn.addEventListener("click", (e: Event) => {
+        if (btn.classList.contains('magic')) {
+            btn.classList.remove('magic');
+            alert('Nope still another alert :(');
+        } else {
+            btn.classList.add('magic');
+            alert('No magic here :(! It was only an alert, maybe if I click again...');
+        }
+    });
+}
